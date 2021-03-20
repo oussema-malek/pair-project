dee02eb5e7f06bcd9f802e39f895288b56e8155d
malekhizaoui <hizaoui.malek.habib@gmail.com>
2021-03-20 09:34:58 +0100

    init

---
+++ main.js
@@ -0,0 +1,183 @@
+var movies = [];
+function makeMovie(title, duration, genre, url, description, img) {
+  return movies.push({
+    title: title,
+    genre: genre,
+    duration: duration,
+    url: url,
+    description: description,
+    img: img
+
+  })
+}
+
+
+ makeMovie('the Map of tiny perfect things', 1.43, 'Romance',
+  'https://www.youtube.com/watch?v=ZOxsfKQWrUg', 'description', 'https://m.media-amazon.com/images/M/MV5BMjFjMTQxZTYtYzdkZi00ZWMyLWJlN2QtY2IzYTc3MzdlZGY5XkEyXkFqcGdeQXVyNjcyNjcyMzQ@._V1_.jpg')
+ makeMovie('yes Day', 1.26, 'Comedie',
+  'https://www.youtube.com/watch?v=Y-3Vr8Ut8d0', 'description', 'https://fr.web.img6.acsta.net/pictures/21/02/15/11/01/1702438.jpg')
+ makeMovie('the girl on the train', 2, 'Comedie',
+  'https://www.youtube.com/watch?v=LE8-4aRf5VQ', 'description', 'https://upload.wikimedia.org/wikipedia/en/3/34/The_Girl_on_The_Train.jpg')
+ makeMovie('Fou de toi', 1.42, 'Comedie',
+  'https://www.youtube.com/watch?v=mri-Nj-Hrsc', 'description', 'https://www.ecranlarge.com/media/cache/1600x1200/uploads/image/001/367/fou-de-toi-affiche-1367022.jpg')
+ makeMovie('sentinelle', 1.20, 'Action',
+  'https://www.youtube.com/watch?v=hEXDWaExQXk', 'description', 'http://mondocine.net/wp-content/uploads/2015/07/la_sentinelle.jpg')
+makeMovie('master', 2.59, 'Action',
+  'https://www.youtube.com/watch?v=UTiXQcrLlv4', 'description', 'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Master_2021_poster.jpg/220px-Master_2021_poster.jpg')
+ makeMovie('red', 2.25, 'Action',
+  'https://www.youtube.com/watch?v=M6hLhA6hsnw', 'description', 'https://fr.web.img4.acsta.net/medias/nmedia/18/78/61/18/19518457.jpg')
+ makeMovie('the dig', 1.50, 'Romance',
+  'https://www.youtube.com/watch?v=JZQz0rkNajo', 'description', 'https://fr.web.img4.acsta.net/pictures/20/12/04/10/45/3843359.jpg')
+ makeMovie('mara', 1.56, 'Drama',
+  'https://www.youtube.com/watch?v=Lv5KUKKwQEw', 'description', 'https://imgsrc.cineserie.com/2018/07/1128181.jpg?ver=1')
+ makeMovie('red Dot', 1.26, 'Action',
+  'https://www.youtube.com/watch?v=t7FwypV69qc', 'description', '')
+ makeMovie('double dad',3.43, 'Comedie',
+  'https://www.youtube.com/watch?v=Yjq-LSl98Lg', 'description', 'https://stockholm-sardou.fr/posters/pics/Red-Dot-Film-2021.jpg')
+ makeMovie('stuck apart', 1.42, 'Action',
+  'https://www.youtube.com/watch?v=fG96ROkdGVA', 'description', 'https://i.ytimg.com/vi/Yjq-LSl98Lg/maxresdefault.jpg')
+ makeMovie('sobat ambyar', 1.42, 'Comedie',
+  'https://www.youtube.com/watch?v=_bnipDHcyf4', 'description', 'https://fr.web.img6.acsta.net/pictures/20/12/24/14/15/4911328.jpg')
+makeMovie('squared love', 2, 'Ramance',
+  'https://www.youtube.com/results?search_query=squared+love', 'description', 'https://pics.filmaffinity.com/Sobat_Ambyar-662111496-mmed.jpg')
+  makeMovie('aelay', 2, 'Comedie',
+  'https://www.youtube.com/watch?v=EY0apWILgkQ', 'description', 'https://upload.wikimedia.org/wikipedia/en/7/7a/Squared_Love_film_poster.png')
+  makeMovie('aelay', 2, 'Romance',
+  'https://www.youtube.com/watch?v=EY0apWILgkQ', 'description', 'https://www.gaumont.fr/files/oeuvrea/0011528/0011528_aff_002_med.jpg')
+makeMovie('paper Lives ', 3 , 'Drama',
+'https://www.youtube.com/watch?v=lXdSaVK-vzs','description','https://i.ytimg.com/vi/ViKaeMA49Nk/maxresdefault.jpg')
+makeMovie('moxie',3,'Comedie',
+'https://www.youtube.com/watch?v=Sf34qI1hjKU','https://images-na.ssl-images-amazon.com/images/I/91fKAW6a0hL.jpg')
+makeMovie('etat esprit',2,'Drama',
+'https://www.youtube.com/watch?v=Gdf1QqBiB_E','https://www.abusdecine.com/wp-content/uploads/etat-d-esprit-bliss-affiche.jpg')
+makeMovie('the Map of tiny perfect things', 1.43, 'Romance',
+'https://www.youtube.com/watch?v=ZOxsfKQWrUg', 'description', 'https://m.media-amazon.com/images/M/MV5BMjFjMTQxZTYtYzdkZi00ZWMyLWJlN2QtY2IzYTc3MzdlZGY5XkEyXkFqcGdeQXVyNjcyNjcyMzQ@._V1_.jpg')
+makeMovie('yes Day', 1.26, 'Comedie',
+'https://www.youtube.com/watch?v=Y-3Vr8Ut8d0', 'description', 'https://fr.web.img6.acsta.net/pictures/21/02/15/11/01/1702438.jpg')
+makeMovie('the girl on the train', 2, 'Comedie',
+'https://www.youtube.com/watch?v=LE8-4aRf5VQ', 'description', 'https://upload.wikimedia.org/wikipedia/en/3/34/The_Girl_on_The_Train.jpg')
+makeMovie('Fou de toi', 1.42, 'Comedie',
+'https://www.youtube.com/watch?v=mri-Nj-Hrsc', 'description', 'https://www.ecranlarge.com/media/cache/1600x1200/uploads/image/001/367/fou-de-toi-affiche-1367022.jpg')
+makeMovie('sentinelle', 1.20, 'Action',
+'https://www.youtube.com/watch?v=hEXDWaExQXk', 'description', 'http://mondocine.net/wp-content/uploads/2015/07/la_sentinelle.jpg')
+makeMovie('master', 2.59, 'Action',
+'https://www.youtube.com/watch?v=UTiXQcrLlv4', 'description', 'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Master_2021_poster.jpg/220px-Master_2021_poster.jpg')
+makeMovie('red', 2.25, 'Action',
+'https://www.youtube.com/watch?v=M6hLhA6hsnw', 'description', 'https://fr.web.img4.acsta.net/medias/nmedia/18/78/61/18/19518457.jpg')
+makeMovie('the dig', 1.50, 'Romance',
+'https://www.youtube.com/watch?v=JZQz0rkNajo', 'description', 'https://fr.web.img4.acsta.net/pictures/20/12/04/10/45/3843359.jpg')
+makeMovie('mara', 1.56, 'Drama',
+'https://www.youtube.com/watch?v=Lv5KUKKwQEw', 'description', 'https://imgsrc.cineserie.com/2018/07/1128181.jpg?ver=1')
+makeMovie('red Dot', 1.26, 'Action',
+'https://www.youtube.com/watch?v=t7FwypV69qc', 'description', '')
+makeMovie('double dad',3.43, 'Comedie',
+'https://www.youtube.com/watch?v=Yjq-LSl98Lg', 'description', 'https://stockholm-sardou.fr/posters/pics/Red-Dot-Film-2021.jpg')
+makeMovie('stuck apart', 1.42, 'Action',
+'https://www.youtube.com/watch?v=fG96ROkdGVA', 'description', 'https://i.ytimg.com/vi/Yjq-LSl98Lg/maxresdefault.jpg')
+makeMovie('sobat ambyar', 1.42, 'Comedie',
+'https://www.youtube.com/watch?v=_bnipDHcyf4', 'description', 'https://fr.web.img6.acsta.net/pictures/20/12/24/14/15/4911328.jpg')
+makeMovie('squared love', 2, 'Ramance',
+'https://www.youtube.com/results?search_query=squared+love', 'description', 'https://pics.filmaffinity.com/Sobat_Ambyar-662111496-mmed.jpg')
+makeMovie('aelay', 2, 'Comedie',
+'https://www.youtube.com/watch?v=EY0apWILgkQ', 'description', 'https://upload.wikimedia.org/wikipedia/en/7/7a/Squared_Love_film_poster.png')
+makeMovie('aelay', 2, 'Romance',
+'https://www.youtube.com/watch?v=EY0apWILgkQ', 'description', 'https://www.gaumont.fr/files/oeuvrea/0011528/0011528_aff_002_med.jpg')
+makeMovie('paper Lives ', 3 , 'Drama',
+'https://www.youtube.com/watch?v=lXdSaVK-vzs','description','https://i.ytimg.com/vi/ViKaeMA49Nk/maxresdefault.jpg')
+makeMovie('moxie',3,'Comedie',
+'https://www.youtube.com/watch?v=Sf34qI1hjKU','https://images-na.ssl-images-amazon.com/images/I/91fKAW6a0hL.jpg')
+makeMovie('etat esprit',2,'Drama',
+'https://www.youtube.com/watch?v=Gdf1QqBiB_E','https://www.abusdecine.com/wp-content/uploads/etat-d-esprit-bliss-affiche.jpg')
+ makeMovie('the Map of tiny perfect things', 1.43, 'Romance',
+  'https://www.youtube.com/watch?v=ZOxsfKQWrUg', 'description', 'https://m.media-amazon.com/images/M/MV5BMjFjMTQxZTYtYzdkZi00ZWMyLWJlN2QtY2IzYTc3MzdlZGY5XkEyXkFqcGdeQXVyNjcyNjcyMzQ@._V1_.jpg')
+ makeMovie('yes Day', 1.26, 'Comedie',
+  'https://www.youtube.com/watch?v=Y-3Vr8Ut8d0', 'description', 'https://fr.web.img6.acsta.net/pictures/21/02/15/11/01/1702438.jpg')
+ makeMovie('the girl on the train', 2, 'Comedie',
+  'https://www.youtube.com/watch?v=LE8-4aRf5VQ', 'description', 'https://upload.wikimedia.org/wikipedia/en/3/34/The_Girl_on_The_Train.jpg')
+ makeMovie('Fou de toi', 1.42, 'Comedie',
+  'https://www.youtube.com/watch?v=mri-Nj-Hrsc', 'description', 'https://www.ecranlarge.com/media/cache/1600x1200/uploads/image/001/367/fou-de-toi-affiche-1367022.jpg')
+ makeMovie('sentinelle', 1.20, 'Action',
+  'https://www.youtube.com/watch?v=hEXDWaExQXk', 'description', 'http://mondocine.net/wp-content/uploads/2015/07/la_sentinelle.jpg')
+makeMovie('master', 2.59, 'Action',
+  'https://www.youtube.com/watch?v=UTiXQcrLlv4', 'description', 'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Master_2021_poster.jpg/220px-Master_2021_poster.jpg')
+ makeMovie('red', 2.25, 'Action',
+  'https://www.youtube.com/watch?v=M6hLhA6hsnw', 'description', 'https://fr.web.img4.acsta.net/medias/nmedia/18/78/61/18/19518457.jpg')
+ makeMovie('the dig', 1.50, 'Romance',
+  'https://www.youtube.com/watch?v=JZQz0rkNajo', 'description', 'https://fr.web.img4.acsta.net/pictures/20/12/04/10/45/3843359.jpg')
+ makeMovie('mara', 1.56, 'Drama',
+  'https://www.youtube.com/watch?v=Lv5KUKKwQEw', 'description', 'https://imgsrc.cineserie.com/2018/07/1128181.jpg?ver=1')
+ makeMovie('red Dot', 1.26, 'Action',
+  'https://www.youtube.com/watch?v=t7FwypV69qc', 'description', '')
+ makeMovie('double dad',3.43, 'Comedie',
+  'https://www.youtube.com/watch?v=Yjq-LSl98Lg', 'description', 'https://stockholm-sardou.fr/posters/pics/Red-Dot-Film-2021.jpg')
+ makeMovie('stuck apart', 1.42, 'Action',
+  'https://www.youtube.com/watch?v=fG96ROkdGVA', 'description', 'https://i.ytimg.com/vi/Yjq-LSl98Lg/maxresdefault.jpg')
+ makeMovie('sobat ambyar', 1.42, 'Comedie',
+  'https://www.youtube.com/watch?v=_bnipDHcyf4', 'description', 'https://fr.web.img6.acsta.net/pictures/20/12/24/14/15/4911328.jpg')
+makeMovie('squared love', 2, 'Ramance',
+  'https://www.youtube.com/results?search_query=squared+love', 'description', 'https://pics.filmaffinity.com/Sobat_Ambyar-662111496-mmed.jpg')
+  makeMovie('aelay', 2, 'Comedie',
+  'https://www.youtube.com/watch?v=EY0apWILgkQ', 'description', 'https://upload.wikimedia.org/wikipedia/en/7/7a/Squared_Love_film_poster.png')
+  makeMovie('aelay', 2, 'Romance',
+  'https://www.youtube.com/watch?v=EY0apWILgkQ', 'description', 'https://www.gaumont.fr/files/oeuvrea/0011528/0011528_aff_002_med.jpg')
+makeMovie('paper Lives ', 3 , 'Drama',
+'https://www.youtube.com/watch?v=lXdSaVK-vzs','description','https://i.ytimg.com/vi/ViKaeMA49Nk/maxresdefault.jpg')
+makeMovie('moxie',3,'Comedie',
+'https://www.youtube.com/watch?v=Sf34qI1hjKU','https://images-na.ssl-images-amazon.com/images/I/91fKAW6a0hL.jpg')
+makeMovie('etat esprit',2,'Drama',
+'https://www.youtube.com/watch?v=Gdf1QqBiB_E','https://www.abusdecine.com/wp-content/uploads/etat-d-esprit-bliss-affiche.jpg')
+
+function displaymovies(arr) {
+  $(".video-film").hide()
+  if (arr === undefined) {
+    arr = movies
+  }
+  $('#ourmovies').html("")
+
+  for (var i = 0; i < arr.length; i++) {
+
+    if ($('#smovie').val() != "") {
+        if ( arr[i].title == $('#smovie').val() )
+        {
+          $('#ourmovies').append("<img data-attribut=" + arr[i].url + "  class='hi' src=" + arr[i].img + ">")
+          $("#ourmovies").find(`[data-attribut='${arr[i].url}']`).click(function(event){
+            var url = event.target.dataset.attribut
+            $("#iframe").attr("src",getIdFromVideoYoutube(url))
+            $(".video-film").show()
+          });    
+            document.getElementById("duration").value = ""
+            document.getElementById("genre").value = ""
+
+        }
+    }
+    
+   else if ($('#smovie').val() == "") {
+         if (($('#duration').val() == 999)|| (Math.trunc(arr[i].duration) == $('#duration').val()))
+        {
+            if ($('#genre').val() == "All" || arr[i].genre == $('#genre').val())
+            {
+                $('#ourmovies').append("<img data-attribut=" + arr[i].url + "  class='hi' src=" + arr[i].img + ">")
+                $("#ourmovies").find(`[data-attribut='${arr[i].url}']`).click(function(event){
+                  var url = event.target.dataset.attribut
+                  $("#iframe").attr("src",getIdFromVideoYoutube(url))
+                  $(".video-film").show()
+                });  }
+        }
+    }
+  }
+}
+
+
+function getIdFromVideoYoutube(video_id) {
+  var video_id = video_id.split('v=')[1];
+  return "https://www.youtube.com/embed/" + video_id
+  }
+displaymovies();
+
+
+
+
+
+
+
+
